77132d6cc6503853f0e06a27a6169db51de33472
caqwal <am.awaludinmuhammad@gmail.com>
2023-08-08 08:34:24 +0700

    update response error

--- src/error/response-error.js
+++ src/error/response-error.js
@@ -3,7 +3,7 @@ class ResponseError extends Error {
     constructor (status, message, details = {}) {
         super(message);
         this.status = status;
-        this.validation_errors = {}
+        this.validation_errors = []
 
         if (details.hasOwnProperty('validation_errors')) {
             this.validation_errors = details.validation_errors.map((item) => {
