b41ad10fba3313d283a09743295b2f0715a51633
caqwal <am.awaludinmuhammad@gmail.com>
2023-08-13 17:33:26 +0700

    add setPrismaArgs for dynamic select

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -22,6 +22,20 @@ const withImageUrl = (data, prop) => {
     }
 }
 
+const setPrismaArgs = (options = {}) => {
+    const args = {}
+    if (options.select) {
+        const select = {};
+        options.select.forEach(field => {
+            select[field] = true; 
+        });
+        args.select = select;
+    }
+
+    return args;
+}
+
 export {
-    withImageUrl
+    withImageUrl,
+    setPrismaArgs
 }
