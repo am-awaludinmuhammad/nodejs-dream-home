3b91b16bafc2caaa83dcfc830f1eafa2aeca76c9
caqwal <am.awaludinmuhammad@gmail.com>
2023-08-13 18:00:10 +0700

    add swagger doc

--- src/config/web.js
+++ src/config/web.js
@@ -4,15 +4,20 @@ import { certificateRouter } from "../routes/certificate-route.js";
 import * as dotenv from 'dotenv';
 import { errorMiddleware } from "../middleware/error-middleware.js";
 import { productRouter } from "../routes/product-route.js";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import apiDocs from "../../docs/apidocs.json" assert { type: "json" }
 
 dotenv.config();
-const api = process.env.API_PREFIX_URL;
-
 export const web = new express();
+
+const api = process.env.API_PREFIX_URL;
+const specs = swaggerJsdoc(apiDocs);
 
 web.use(express.static('public'));
 web.use(express.json());
 
+web.use(`${api}/api-docs`, swaggerUi.serve, swaggerUi.setup(specs));
 web.use(`${api}/categories`, categoryRouter);
 web.use(`${api}/certificates`, certificateRouter);
 web.use(`${api}/products`, productRouter);
