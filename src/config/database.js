9f0ca181f5be5de09d66b053eb42686d533c28fa
caqwal <am.awaludinmuhammad@gmail.com>
2023-08-17 12:15:28 +0700

    update query result for thumbnail

--- src/config/database.js
+++ src/config/database.js
@@ -1,27 +1,72 @@
-import { PrismaClient } from "@prisma/client";
-import { logger } from "./logger.js";
+import {
+    PrismaClient
+} from "@prisma/client";
 
-export const prisma = new PrismaClient({
-    log: [
-        {
+import {
+    logger
+} from "./logger.js";
+
+const {
+    IMG_URL
+} = process.env
+
+const prismaClient = new PrismaClient({
+    log: [{
             emit: 'event',
             level: 'query',
-          },
-          {
+        },
+        {
             emit: 'event',
             level: 'error',
-          },
-          {
+        },
+        {
             emit: 'event',
             level: 'info',
-          },
-          {
+        },
+        {
             emit: 'event',
             level: 'warn',
         },
     ]
 });
 
-prisma.$on('error', (e) => {
+const prisma = prismaClient.$extends({
+    result: {
+        category: {
+            thumbnail: {
+                needs: {
+                    thumbnail: true
+                },
+                compute(category) {
+                    if (!category.thumbnail) {
+                        return `${IMG_URL}/default/no_image.png`;
+                    }
+
+                    return `${IMG_URL}/uploads/${category.thumbnail}`;
+                },
+            },
+        },
+        product: {
+            thumbnail: {
+                needs: {
+                    thumbnail: true
+                },
+                compute(product) {
+                    if (!product.thumbnail) {
+                        return `${IMG_URL}/default/no_image.png`;
+                    }
+
+                    return `${IMG_URL}/uploads/${product.thumbnail}`;
+                },
+            },
+        },
+    },
+});
+
+prismaClient.$on('error', (e) => {
     logger.error(e);
 })
+
+export {
+    prisma
+}
