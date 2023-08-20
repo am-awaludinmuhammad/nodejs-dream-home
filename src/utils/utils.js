6e43d9bafcaae8522df8cad99e0b09b3a8230100
caqwal <am.awaludinmuhammad@gmail.com>
2023-08-20 08:29:42 +0700

    remove withImageUrl

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,27 +1,3 @@
-import { NO_IMAGE } from "./const.js";
-
-const withImageUrl = (data, prop) => {
-    if (data instanceof Array) {
-        return data.map((obj) => {
-            if (obj[prop]) {
-                obj[prop] = `${process.env.IMG_URL}/uploads/${obj[prop]}`;
-            } else {
-                obj[prop] = `${process.env.IMG_URL}/default/${NO_IMAGE}`;
-            }
-
-            return obj;
-        });
-    } else if (data instanceof Object) {
-        if (data[prop]) {
-            data[prop] = `${process.env.IMG_URL}/uploads/${data[prop]}`;
-        } else {
-            data[prop] = `${process.env.IMG_URL}/default/${NO_IMAGE}`;
-        }
-
-        return data;
-    }
-}
-
 const setPrismaArgs = (options = {}) => {
     const args = {}
     if (options.select) {
@@ -36,6 +12,5 @@ const setPrismaArgs = (options = {}) => {
 }
 
 export {
-    withImageUrl,
     setPrismaArgs
 }
